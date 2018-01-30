7d3031ded83a3cd6b661d37fc9e244cab8d90334
THINH NGUYEN <thinhnguyen74@gmail.com>
2018-01-30 01:41:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -79,6 +79,8 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
   if (!cardNumber) {
-    console.log ("")
+    console.log ("Sorry, we don't have a credit card on file for you.")
   }
+  
+  
 }
